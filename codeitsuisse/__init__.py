0e5bf1d6db9e66caea5db076e3e5501776295040
Terry <57288769+tlwuab@users.noreply.github.com>
2020-09-26 12:33:00 +0800

    Test

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.secret_message
 import codeitsuisse.routes.revisit_geometry
-
+import codeitsuisse.routes.magic_fruit_basket
 
 
--- codeitsuisse/routes/magic_fruit_basket.py
+++ codeitsuisse/routes/magic_fruit_basket.py
@@ -8,16 +8,17 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/fruitbasket', methods=['POST'])
-def evaluateSecretMessage():
+def evaluateFruitWeight():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     #inputValue = data.get("input");
     result = 0
-    result = result + data["maApple"]*data["maApple"]*10
-    result = result + data["maWatermelon"]*data["maWatermelon"]*10
-    result = result + data["maBanana"*data["maBanana"]*10
+    result = result + (data["maApple"]*data["maApple"]*10)
+    result = result + (data["maWatermelon"]*data["maWatermelon"]*10)
+    result = result + (data["maBanana"]*data["maBanana"]*10)
+
     logging.info("My result :{}".format(result))
-    return jsonify(data);
+    return jsonify(result);
 
 
 
