6bbb9a51c77c72a31c6d117a997aec2c7ad23eac
Terry <57288769+tlwuab@users.noreply.github.com>
2020-09-26 14:14:48 +0800

    Test Olympoad Babylon

--- codeitsuisse/routes/olypiad_babylon.py
+++ codeitsuisse/routes/olypiad_babylon.py
@@ -8,7 +8,7 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
-def evaluateSecretMessage():
+def evaluateOptimal():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     #inputValue = data.get("input");
@@ -20,18 +20,21 @@ def evaluateSecretMessage():
 def optimal(d):
     nBooks = d["numberOfBooks"]
     nDays = d["numberOfDays"]
-    sortedBook = d["books"].sort()
-    sortedDays = d["days"].sort()
+    sortedBook = d["books"]
+    sortedBook.sort(reverse=True)
+    sortedDays = d["days"]
+    sortedDays.sort(reverse=True)
     optimalBooks = 0
-    j = nBooks-1
-    for i in range(len(sortedDays)):
-        if optimalBooks == nBooks:
-            break
-        if sortedBook[i] >= sortedBook[j]:
-            sortedBook[i] = sortedBook[i] - sortedBook[j]
-            i = i - 1
-            j = j - 1
+    start = 0
+    end = nBooks - 1
+    for i in range(nDays): 
+        sortedDays[i] = sortedDays[i] - sortedBook[start]
+        optimalBooks = optimalBooks + 1
+        start = start + 1
+        while(sortedDays[i]>=sortedBook[end]):
+            sortedDays[i] = sortedDays[i] - sortedBook[end]
             optimalBooks = optimalBooks + 1
+            end = end - 1
     return { "optimalNumberOfBooks" : optimalBooks }
 
 
