15187299556ad68213272798ff8b9ba7799b985a
Terry <57288769+tlwuab@users.noreply.github.com>
2020-09-26 18:31:18 +0800

    Update magic_fruit_basket.py

--- codeitsuisse/routes/magic_fruit_basket.py
+++ codeitsuisse/routes/magic_fruit_basket.py
@@ -12,7 +12,7 @@ def evaluateFruitWeight():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     #inputValue = data.get("input");
-    guess = 6000
+    guess = 6933
     result = "{}".format(guess)
     return result;
 
