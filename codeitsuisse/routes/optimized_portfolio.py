8f06a20420d5a2f306de7b8ece588dd6e505b27f
Terry <57288769+tlwuab@users.noreply.github.com>
2020-09-27 01:51:25 +0800

    Update optimized_portfolio.py

--- codeitsuisse/routes/optimized_portfolio.py
+++ codeitsuisse/routes/optimized_portfolio.py
@@ -20,6 +20,11 @@ def evaluateOptimizedPortfolio():
     logging.info("My result :{}".format(result))
     return jsonify(result)
 
+def myround(num):
+    if(round(num,2)-int(num)>=0.45):
+        num = int(num) + 1
+    return num
+
 def opti(d):
     min_OHR = 1
     min_NFC = 0
@@ -32,7 +37,7 @@ def opti(d):
 
     for i in range(len(d["IndexFutures"])):
         if round(d["IndexFutures"][i]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][i]["FuturePrcVol"], 3) == min_OHR:
-            min_NFC = round(min_OHR*d["Portfolio"]["Value"]/(d["IndexFutures"][i]["IndexFuturePrice"]*d["IndexFutures"][i]["Notional"]))
+            min_NFC = myround((d["IndexFutures"][i]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][i]["FuturePrcVol"])*d["Portfolio"]["Value"]/(d["IndexFutures"][i]["IndexFuturePrice"]*d["IndexFutures"][i]["Notional"]))
             result.append({ "HedgePositionName" : d["IndexFutures"][i]["Name"],"OptimalHedgeRatio" : min_OHR,"NumFuturesContract" : min_NFC })
     
     return result
