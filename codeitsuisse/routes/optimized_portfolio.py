1d1669a6146ba7eff39aca8e222a90094a8b4248
Terry <57288769+tlwuab@users.noreply.github.com>
2020-09-27 01:21:16 +0800

    Update optimized_portfolio.py

--- codeitsuisse/routes/optimized_portfolio.py
+++ codeitsuisse/routes/optimized_portfolio.py
@@ -22,13 +22,18 @@ def opti(d):
     min_OHR = 1
     min_NFC = 0
     best_option = 0
+    result = []
     for i in range(len(d["IndexFutures"])):
-        if d["IndexFutures"][i]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][i]["FuturePrcVol"] < min_OHR:
+        if round(d["IndexFutures"][i]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][i]["FuturePrcVol"], 3) <= min_OHR:
             best_option = i
     min_OHR = round(d["IndexFutures"][best_option]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][best_option]["FuturePrcVol"],3)
-    min_NFC = int(round(min_OHR*d["Portfolio"]["Value"]/(d["IndexFutures"][best_option]["IndexFuturePrice"]*d["IndexFutures"][best_option]["Notional"]),0))
-    return { "HedgePositionName" : d["IndexFutures"][best_option]["Name"], "OptimalHedgeRatio" : min_OHR, "NumFuturesContract" : min_NFC }
+
+    for i in range(len(d["IndexFutures"])):
+        if round(d["IndexFutures"][i]["CoRelationCoefficient"]*d["Portfolio"]["SpotPrcVol"]/d["IndexFutures"][i]["FuturePrcVol"], 3) == min_OHR:
+            min_NFC = int(round(min_OHR*d["Portfolio"]["Value"]/(d["IndexFutures"][i]["IndexFuturePrice"]*d["IndexFutures"][i]["Notional"]),0))
+            result.append({ "HedgePositionName" : d["IndexFutures"][i]["Name"], "OptimalHedgeRatio" : min_OHR, "NumFuturesContract" : min_NFC })
     
+    return result
 
 
 
